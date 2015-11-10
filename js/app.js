a2e3782c099df99b50ef70b537b469193565c309
AdamFielding <AdamFielding@users.noreply.github.com>
2015-11-10 12:17:32 +0000

    finished angular general

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
 angular.module('app', [])
 .controller(githubController, ['$scope', '$http', function($scope, $http) {
 
-	var loadRepos = function () {
 		$http.get("https://api.github.com/orgs/WASdev/repos?callback=loadRepos&per_page=1000")
 			.success(function(data) {
 				$scope.repoData = data;
-			});
-	}
+		});
 }]);
