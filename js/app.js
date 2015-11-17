7612c2035c50e730dbf1e3747350ca4da1253253
AdamFielding <AdamFielding@users.noreply.github.com>
2015-11-17 13:42:31 +0000

    attempt to fix views

--- js/app.js
+++ js/app.js
@@ -3,9 +3,11 @@ var app = angular.module("app", ['ngRoute']);
 app.config(function ($routeProvider) {
     $routeProvider
     .when('/', {
+        controller: 'MainController',
         templateUrl: 'views/all.html'
     })
     .when('/samples', {
+        controller: 'MainController',
         templateUrl: 'views/samples.html'
     })
     .otherwise({
