1628723360e6e36349a9ec2c003eb84dce4de573
AdamFielding <AdamFielding@users.noreply.github.com>
2015-12-04 10:35:22 +0000

    change the api request to wasdev

--- js/services/github.js
+++ js/services/github.js
@@ -1,8 +1,7 @@
 /*Adam Fielding's http service calling the GitHub API for the github repo page */
 
 app.factory('github', ['$http', function($http) {
-  return $http.get('https://api.github.com/users/adamfielding/repos?per_page=100', { cache: true})
-  //https://api.github.com/orgs/WASdev/repos?per_page=100
+  return $http.get('https://api.github.com/orgs/WASdev/repos?per_page=100', { cache: true})
             //success may be deprecated, see $http service docs
             .success (function(data) {
                 return data;
