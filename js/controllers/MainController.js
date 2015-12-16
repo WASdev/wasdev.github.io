b4301ca132a1bb6d8bcc60fc3a427769920696ab
AdamFielding <AdamFielding@users.noreply.github.com>
2015-12-16 13:30:23 +0000

    fix error

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,8 +2,8 @@
 
 app.controller('MainController', ['$scope', 'github', function($scope, github) {
 
-    github.success(function(response) {
-        $scope.repos = response;
+    github.success(function(data) {
+        $scope.repos = data;
 
         $scope.click = function(filter) {
             $scope.myFilter = filter;
@@ -28,9 +28,6 @@ app.controller('MainController', ['$scope', 'github', function($scope, github) {
         $scope.select= function(i) {
             $scope.selectedIndex=i;
         };
-
-
-//code for the tag system
 
         //create categories array
         $scope.arrayOfWords = [];
