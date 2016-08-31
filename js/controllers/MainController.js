950a94ffac3928bad9708b33d39ef5d83b3913ee
BillyD73 <Billy.Dawson3@uk.ibm.com>
2016-08-31 13:43:50 +0100

    Initial commit

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -129,7 +129,7 @@ angular.module('app')
             github.getGitHubData(url, function(response) {
                 repos = repos.concat(response.data);
                 ////////////
-                alert(response.data);
+                alert(response);
                 ////////////
                 if (response.headers('link').indexOf("next") >= 0) {
                     pageNumber = pageNumber + 1;
