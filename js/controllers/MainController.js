78a246bda16956235fbe4854fc72dbd71f2cfcce
BillyD73 <Billy.Dawson3@uk.ibm.com>
2016-08-31 15:41:10 +0100

    Initial commit

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -125,11 +125,12 @@ angular.module('app')
 
         //getting the data
         getAllGitHubData = function() {
+            if (bool=0){
             url = "https://api.github.com/orgs/WASdev/repos?per_page=90&page=" + pageNumber;
-            $.getJSON('https://raw.githubusercontent.com/BillyD73/billyd73.github.io/master/test.json', function(data) {
-            //data is the JSON string
-            });
-            alert(data);
+            url2 = "https://raw.githubusercontent.com/BillyD73/billyd73.github.io/master/test.json" + pageNumber;
+          }
+          bool = 1;
+
             github.getGitHubData(url, function(response) {
                 repos = repos.concat(response.data);
                 if (response.headers('link').indexOf("next") >= 0) {
@@ -144,6 +145,7 @@ angular.module('app')
             });
         }
 
+        bool = 0;
         getAllGitHubData();
 
 }]);
