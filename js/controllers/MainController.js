a9b3d8a5a79291a120a8f62ab93f691c2cc61936
BillyD73 <Billy.Dawson3@uk.ibm.com>
2016-09-02 11:50:55 +0100

    Pagination fix

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -138,23 +138,21 @@ angular.module('app')
 
             github.getGitHubData(url, function(response) {
                 repos = repos.concat(response.data);
-                if (response!==null){
-                if (response.headers('link').indexOf("next") >= 0) { //This is apparently null with my file - pagination issues
+                if (response.headers==null){
+                    generateFilters();
+                    generateTags();
+                    pushToArray();
+                  }
+                else if (response.headers('link').indexOf("next") >= 0) { //This is apparently null with my file - pagination issues
                     pageNumber = pageNumber + 1;
                     getAllGitHubData();
                 }
-               else {
+               else {/////////////////////
                     generateFilters();
                     generateTags();
                     pushToArray();
                 }
             }
-            else {
-              generateFilters();
-              generateTags();
-              pushToArray();
-                 }
-          }
         );
         }
 
