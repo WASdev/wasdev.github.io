dcf74cd9429e19774fb83badadff0659b3c8b9c0
BillyD73 <Billy.Dawson3@uk.ibm.com>
2016-09-22 14:20:42 +0100

    loader

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -6,7 +6,7 @@ angular.module('app')
         pageNumber = 1;
         repoLocation = "https://api.github.com/orgs/WASdev/repos";
         var img = new Image();
-        var div = document.getElementById('githubCategories');
+        var div = document.getElementById('pnext-main');
         img.onload = function() {
         div.appendChild(img);
         };
